4e672690fd0576a1475265a5512e4745737e1403
PeacefulWindy <a1056419426@gmail.com>
2025-05-10 21:19:11 +0800

    重新改动工具，并添加说明和例子

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # ConfigTools
 配置表生成复制工具
+
+---
+
+这是一个excel表转json/xml/lua的工具。
+
+PS：仅支持utf8的excel表
+
+---
+### 表格式
+
+第1行是字段，原则上不允许除数字和整数之外的字符，除了字段第1个字符的特殊含义：
+1. 不生成该字段(#)
+2. 仅客户端生成(!)
+3. 仅服务端($)
+
+第2行是数据类型，支持以下的类型：
+1. bool
+2. int
+3. float
+4. json
+
+第3行是注释(生成时不会导出)
+
+第4~N行是数据，其中第1列是主键，最好是int或string类型，避免可能存在的问题。
+
+具体的例子可参考excel/Test.xlsx
+
+### config.json字段说明
+---
+
+input：放置excel的文件夹目录
+
+output：生成json/xml/lua配置的临时目录（会自动在output指定的位置添加client和server目录）
+
+move：可以指定client/server和对应的格式，自动复制到对应的目录。（可缺省参数）
+
+具体的例子可参考config.json
+
+---
+
+### 运行方式
+双击run.bat或runOnce.bat
+
+其中，run.bat是监测文件变化自动生成，runOnce.bat立刻运行1次。
+
+PS：将读取上一层目录的config.json的配置。
+
+---
+
+### 例子运行方式
+双击example.bat或exampleOnce.bat
+
+PS：将读取本目录的config.json的配置。
--- generate.py
+++ generate.py
@@ -1,197 +1,289 @@
 import os
+import sys
 import openpyxl
 import json
 from slpp import slpp as lua
 import xml.etree.ElementTree as ET
 import time
 import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import colorama
+import re
 
-def export_to_lua(sheet_name, data, output_dir):
-    output_file = os.path.join(output_dir, f"{sheet_name}.lua")
-    with open(output_file, 'w', encoding='utf-8') as f:
-        lua_str = lua.encode(data)
-        f.write("return "+lua_str)
+def exportToLua(sheetName, data, outputDir):
+    outputFileName = os.path.join(outputDir, f"{sheetName}.lua")
+    with open(outputFileName, 'w', encoding='utf-8') as f:
+        luaStr = lua.encode(data)
+        f.write("return "+luaStr)
 
-def export_to_json(sheet_name, data, output_dir):
-    output_file = os.path.join(output_dir, f"{sheet_name}.json")
-    with open(output_file, 'w', encoding='utf-8') as f:
+def exportToJson(sheetName, data, outputDir):
+    outputFileName = os.path.join(outputDir, f"{sheetName}.json")
+    with open(outputFileName, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=None,separators=(",", ":"))
 
-def export_dict_to_xml(parent, data):
+def exportDictToXml(parent, data):
     if isinstance(data, dict):
         for key, value in data.items():
             if isinstance(value, dict):
-                sub_element = ET.SubElement(parent, str(key))
-                export_dict_to_xml(sub_element, value)
+                subElement = ET.SubElement(parent, str(key))
+                exportDictToXml(subElement, value)
             else:
-                sub_element = ET.SubElement(parent, str(key))
-                sub_element.text = str(value)
+                subElement = ET.SubElement(parent, str(key))
+                subElement.text = str(value)
 
-def export_to_xml(sheet_name, data, output_dir):
-    output_file = os.path.join(output_dir, f"{sheet_name}.xml")
+def exportToXml(sheetName, data, outputDir):
+    outputFileName = os.path.join(outputDir, f"{sheetName}.xml")
     root = ET.Element("xml")
     for key, value in data.items():
-        export_dict_to_xml(root, {key: value})
+        exportDictToXml(root, {key: value})
     tree = ET.ElementTree(root)
-    tree.write(output_file, encoding='utf-8')
+    tree.write(outputFileName, encoding='utf-8')
 
 formatFunc={
-    "lua":export_to_lua,
-    "json":export_to_json,
-    "xml":export_to_xml,
+    "lua":exportToLua,
+    "json":exportToJson,
+    "xml":exportToXml,
 }
 
-def process_excel(filepath,outputPath):
-    wb = openpyxl.load_workbook(filepath,data_only=True)
+keyPattern = re.compile(r'^[A-Za-z0-9]+$')
+
+sheets={}
+
+def generateExcel(filePath,outputPath):
+    print("--------------------")
+    print("open %s"%filePath)
+
+    wb = openpyxl.load_workbook(filePath,data_only=True)
+
+    outputClientRootDir = os.path.join(outputPath, "client")
+    outputServerRootDir = os.path.join(outputPath, "server")
 
     sheetNames=[]
     for sheet in wb.worksheets:
         if not sheet.title.startswith("#"):
             continue
         
-        sheet_name = sheet.title.strip("#")  # 去掉表名中的第一个特殊符号
-        print("sheetName:"+sheet_name)
+        sheetName = sheet.title[1:]
+        if sheetName in sheets:
+            print("%srepeat sheet:%s\nfile:%s\nanotherFile:%s\n" % (colorama.Fore.RED,sheetName,sheets[sheetName],filePath))
+            return
+        
+        sheets[sheetName]=filePath
+        print("sheetName:%s" % sheetName)
 
-        output_client_base_dir = os.path.join(outputPath, "client")
-        output_server_base_dir = os.path.join(outputPath, "server")
         for ext,func in formatFunc.items():
-            output_client_dir = os.path.join(output_client_base_dir, ext)
-            if not os.path.exists(output_client_dir):
-                os.makedirs(output_client_dir)
+            outputClientDir = os.path.join(outputClientRootDir, ext)
+            if not os.path.exists(outputClientDir):
+                os.makedirs(outputClientDir)
             
-            output_server_dir = os.path.join(output_server_base_dir, ext)
-            if not os.path.exists(output_server_dir):
-                os.makedirs(output_server_dir)
+            outputServerDir = os.path.join(outputServerRootDir, ext)
+            if not os.path.exists(outputServerDir):
+                os.makedirs(outputServerDir)
             
-            client_data_dict = {}
-            server_data_dict = {}
+            clientData = {}
+            serverData = {}
+
+            ids=[]
+            keys=[]
             
             for row in range(4, sheet.max_row + 1):
-                first_col_value = sheet.cell(row=row, column=1).value
-                if not first_col_value:
+                id = sheet.cell(row=row, column=1).value
+                if not id:
                     continue
+
+                if id in ids:
+                    print("%srepeat id:%s\nfile:%s\nsheetName:%s\n" % (colorama.Fore.RED,id,filePath,sheetName))
+                    return
                 
-                client_row_data = {}
-                server_row_data = {}
+                ids.append(id)
+
+                clientRowData = {}
+                serverRowData = {}
+
                 for col in range(1, sheet.max_column + 1):
-                    col_key = sheet.cell(row=1, column=col).value
-                    col_type = sheet.cell(row=2,column=col).value
-                    col_value = sheet.cell(row=row, column=col).value
+                    colKey = sheet.cell(row=1, column=col).value
+                    if not colKey or len(colKey) <= 0:
+                        continue
 
-                    if col_type == "json":
-                        if col_value:
-                            col_value=json.loads(col_value)
-                        else:
-                            col_value={}
-                    elif col_type == "int":
-                        if not col_value:
-                            col_value=0
-                    elif col_type == "float":
-                        if not col_value:
-                            col_value=0.0
-                    elif col_type == "string":
-                        if not col_value:
-                            col_value=""
-                    elif col_type == "bool":
-                        if not col_value:
-                            col_value=False
+                    keyFirstChar=colKey[0]
+
+                    if keyFirstChar == "#":
+                        continue
+
+                    if keyFirstChar == "!" or keyFirstChar == "$":
+                        key=colKey[1:]
+                    else:
+                        key=colKey
                     
-                    if col_key and not col_key.startswith("#"):
-                        if not col_key.startswith("!"):
-                            key=col_key
-                            if key.startswith("$"):
-                                key=col_key[1:]
-                            client_row_data[key] = col_value
-                        
-                        if not col_key.startswith("$"):
-                            key=col_key
-                            if key.startswith("!"):
-                                key=col_key[1:]
-                            server_row_data[key] = col_value
-                
-                client_data_dict[first_col_value] = client_row_data
-                server_data_dict[first_col_value] = server_row_data
-            
-            start_time = time.time()
-            func(sheet_name, client_data_dict,output_client_dir)
-            func(sheet_name, server_data_dict,output_server_dir)
-            end_time = time.time()
-            execution_time = end_time - start_time
-            print(ext + " generate time:"+str(execution_time))
-        sheetNames.append(sheet_name)
-    print("")
+                    if not bool(keyPattern.fullmatch(key)):
+                        print("%sinvalid key:%s\nfile:%s\nsheet:%s" % (colorama.Fore.RED,key,filePath,sheetName))
+                        return
 
+                    colType = sheet.cell(row=2,column=col).value
+                    colValue = sheet.cell(row=row, column=col).value
+
+                    if colType == "json":
+                        if colValue:
+                            try:
+                                colValue=json.loads(colValue)
+                            except ValueError as e:
+                                print("%sinvalid json:%s\nfile:%s\nsheet:%s\nid:%s\nkey:%s" % (colorama.Fore.RED,colValue,filePath,sheetName,id,key))
+                                return
+                        else:
+                            colValue={}
+                    elif colType == "int":
+                        if not colValue:
+                            colValue=0
+                    elif colType == "float":
+                        if not colValue:
+                            colValue=0.0
+                    elif colType == "string":
+                        if not colValue:
+                            colValue=""
+                    elif colType == "bool":
+                        if not colValue:
+                            colValue=False
+                    
+                    if keyFirstChar != "!":
+                        clientRowData[key]=colValue
+                    
+                    if keyFirstChar != "$":
+                        serverRowData[key]=colValue
+
+                clientData[id] = clientRowData
+                serverData[id] = serverRowData
+            
+            startTime = time.time()
+            func(sheetName, clientData,outputClientDir)
+            func(sheetName, serverData,outputServerDir)
+            endTime = time.time()
+            execTime = endTime - startTime
+            print("%s%s generate time:%.2fs" % (colorama.Fore.GREEN,ext,execTime))
+        
+        sheetNames.append(sheetName)
+
+    print("--------------------")
+    print("")
     return sheetNames
 
-def move(json_data,type):
-    moveRoot=json_data["move"]
-    if not type in moveRoot:
+def move(configData,target):
+    moveRoot=configData["move"]
+    if not target in moveRoot:
         return
     
-    if "client" in moveRoot:
-        outputClientPath=os.path.join(json_data["output"],type)
-        for format in moveRoot[type]:
-            outputClientFormatPath=os.path.join(outputClientPath,format)
-            if not os.path.exists(outputClientFormatPath):
-                os.makedirs(outputClientFormatPath)
+    inputPath=os.path.abspath(os.path.join(configData["output"],target))
+    
+    for format in moveRoot[target]:
+        inputTargetPath=os.path.join(inputPath,format)
+        
+        for it in moveRoot[target][format]:
+            outPutPath=os.path.abspath(it)
+            if not os.path.exists(outPutPath):
+                os.makedirs(outPutPath)
             
-            for it in moveRoot[type][format]:
-                if not os.path.exists(it):
-                    os.makedirs(it)
-                
-                shutil.copytree(outputClientFormatPath, it,dirs_exist_ok=True)
+            print("%s => %s" % (inputTargetPath,outPutPath))
+            shutil.copytree(inputTargetPath,outPutPath,dirs_exist_ok=True)
 
-def main():
-    json_data={}
-    with open("../config.json", "r", encoding="utf-8") as file:
-        json_data = json.load(file)
+def main(configData):
+    inputPath=os.path.abspath(configData["input"])
+    outputPath=os.path.abspath(configData["output"])
 
-    if not os.path.exists(json_data["output"]):
-        os.makedirs(json_data["output"])
-
-    sheetNames=[]
-    for root, dirs, files in os.walk(json_data["input"]):
-        for file in files:
-            if not file.startswith("~") and file.endswith(".xlsx"):
-                filepath = os.path.join(root, file)
-                print(filepath)
-                ret=process_excel(filepath,json_data["output"])
-                sheetNames.extend(ret)
-    
-    for ext,func in formatFunc.items():
-        output_client_base_dir = os.path.join(json_data["output"], "client",ext)
-        output_server_base_dir = os.path.join(json_data["output"], "server",ext)
-        data={}
-        
-        if os.path.exists(output_client_base_dir):
-            for it in sheetNames:
-                if os.path.exists(os.path.join(output_client_base_dir,it+"."+ext)):
-                    data[it]=True
-            func("init",data,output_client_base_dir)
-            data.clear()
-        
-        if os.path.exists(output_server_base_dir):
-            for it in sheetNames:
-                if os.path.exists(os.path.join(output_server_base_dir,it+"."+ext)):
-                    data[it]=True
-            func("init",data,output_server_base_dir)
-
-    if not "move" in json_data:
-        return
-    
-    print("will move config...")
-    print("move client config...")
-    move(json_data,"client")
-    print("move server config...")
-    move(json_data,"server")
-    print("move config finish!")
+    print("inputPath:%s"%inputPath)
+    print("outputPath:%s"%outputPath)
     print()
 
+    print("generate excel")
+    print()
+
+    sheetDatas=[]
+    for root, dirs, files in os.walk(inputPath):
+        for file in files:
+            if not file.startswith("~") and file.endswith(".xlsx"):
+                filePath = os.path.join(root, file)
+                ret=generateExcel(filePath,outputPath)
+                if not ret:
+                    return
+                
+                sheetDatas.extend(ret)
+    
+    for ext,func in formatFunc.items():
+        outputClientRootDir = os.path.join(outputPath, "client",ext)
+        outputServerBaseDir = os.path.join(outputPath, "server",ext)
+        data={}
+        
+        if os.path.exists(outputClientRootDir):
+            for it in sheetDatas:
+                if os.path.exists(os.path.join(outputClientRootDir,it+"."+ext)):
+                    data[it]=True
+            func("init",data,outputClientRootDir)
+            data.clear()
+        
+        if os.path.exists(outputServerBaseDir):
+            for it in sheetDatas:
+                if os.path.exists(os.path.join(outputServerBaseDir,it+"."+ext)):
+                    data[it]=True
+            func("init",data,outputServerBaseDir)
+
+    if not "move" in configData:
+        return
+    
+    print("move client config")
+    move(configData,"client")
+    print("move server config")
+    move(configData,"server")
+    print()
+
+def run(configData):
+    startTime = time.time()
+    main(configData)
+    endTime = time.time()
+    execTime = endTime - startTime
+    print("%stotal time:%.2fs" % (colorama.Fore.GREEN,execTime))
+    print("done!")
+
+configData={}
+class FileMonitor(FileSystemEventHandler):
+    def on_modified(self, event):
+        if not event.is_directory:
+            os.system('cls' if os.name == 'nt' else 'clear')
+            run(configData)
 
 if __name__ == "__main__":
-    start_time = time.time()
-    main()
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print("total time:"+str(execution_time))
-    print("Done!")
+    colorama.init(autoreset=True)
+
+    configPath=os.path.abspath(sys.argv[1])
+    if not os.path.exists(configPath):
+        print("%snot found config file:%s" % (colorama.Fore.RED,configPath))
+        exit(-1)
+
+    with open(configPath, "r", encoding="utf-8") as file:
+        configData = json.load(file)
+    
+    inputPath=os.path.abspath(configData["input"])
+    if not os.path.exists(inputPath):
+        print("%sinvalid input path:%s" % (colorama.Fore.RED,inputPath))
+        exit(-1)
+    
+    outputPath=os.path.abspath(configData["output"])
+    if not os.path.exists(outputPath):
+        os.makedirs(outputPath)
+    
+    if len(sys.argv) > 2 and sys.argv[2] == "-once":
+        run(configData)
+    else:
+        fileMonitorHandle=FileMonitor()
+        fileMonitorObserver = Observer()
+        fileMonitorObserver.schedule(fileMonitorHandle, inputPath, recursive=False)
+        fileMonitorObserver.start()
+
+        print("%sauto generate config service start!" % colorama.Fore.GREEN)
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            fileMonitorObserver.stop()
+            print("auto generate config service stop!")
+
+        fileMonitorObserver.join()
